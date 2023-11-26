5c4086200109594ed2bc9830c8f89e76ad179740
Wjz <wangjunzhi1220@outlook.com>
2023-11-26 20:16:34 +0800

    element安装及颜色修改

--- xtx/src/App.vue
+++ xtx/src/App.vue
@@ -6,7 +6,14 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
